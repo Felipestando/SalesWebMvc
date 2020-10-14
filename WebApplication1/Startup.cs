69e76cbaed3f910bceb6b40439e82e0244f9f294
Felipe Vieira <felipewc@hotmail.com>
2020-10-14 17:00:30 -0300

    Formatação

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
 using WebApplication1.Services;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 namespace WebApplication1
 {
@@ -50,6 +52,14 @@ namespace WebApplication1
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
             public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
+            var enUS = new CultureInfo("en-US");
+            var localization = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(enUS),
+                SupportedCultures = new List<CultureInfo> { enUS },
+                SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
